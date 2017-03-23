9fc772bc611265fedce132a313efc7f171036d6c
Jonathan <jonathandavis@gilsons.org>
2017-03-23 13:30:08 -0400

    prompt for install

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 
 if ! type "stow" > /dev/null; then
-    xterm -e pacaur -S stow
+    echo "Please install GNU Stow"
+    return
 fi
 for f in $(find ./ -maxdepth 1 -type d | grep -Ev '.git|config' | cut -d '/' -f 2 )
     stow -R $f -t ~/
