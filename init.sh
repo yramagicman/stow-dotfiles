adcfbd547195c760f5ea1f57b24a417ec9b09409
Jonathan <jonathandavis@gilsons.org>
2017-03-24 02:05:01 -0400

    switch the shell to the right zsh

--- init.sh
+++ init.sh
@@ -4,9 +4,16 @@ if ! type "stow" > /dev/null; then
     echo "Please install GNU Stow"
     return
 fi
+
 for f in $(find ./ -maxdepth 1 -type d | grep -Ev '.git|config' | cut -d '/' -f 2 )
     stow -R $f -t ~/
 
 stow -R config -t ~/.config
 
+
+split=("${(@s#/#)SHELL}")
+shell=$split[-1]
+if [[ $shell != 'zsh' ]]; then
+    chsh -s $(which zsh)
+fi
 source ~/.zshrc
