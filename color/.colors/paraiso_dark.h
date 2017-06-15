08c83f263a28f4b3aa209d313cd8a8c79c88799c
Jonathan <jonathandavis@gilsons.org>
2017-06-15 00:45:29 -0400

    another colorscheme

---
+++ color/.colors/paraiso_dark.h
@@ -0,0 +1,44 @@
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+
+  /* 8 normal colors */
+  [0] = "#2f1e2e", /* black   */
+  [1] = "#ef6155", /* red     */
+  [2] = "#48b685", /* green   */
+  [3] = "#fec418", /* yellow  */
+  [4] = "#06b6ef", /* blue    */
+  [5] = "#815ba4", /* magenta */
+  [6] = "#5bc4bf", /* cyan    */
+  [7] = "#a39e9b", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#776e71", /* black   */
+  [9]  = "#ef6155", /* red     */
+  [10] = "#48b685", /* green   */
+  [11] = "#fec418", /* yellow  */
+  [12] = "#06b6ef", /* blue    */
+  [13] = "#815ba4", /* magenta */
+  [14] = "#5bc4bf", /* cyan    */
+  [15] = "#e7e9db", /* white   */
+
+  /* special colors */
+  [256] = "#2f1e2e", /* background */
+  [257] = "#a39e9b", /* foreground */
+};
+
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor
+ */
+static unsigned int defaultfg = 257;
+static unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 257;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
