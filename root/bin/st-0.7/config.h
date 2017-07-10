60724dd3de631ea0a670a809ced06a814432275d
Jonathan <jonathandavis@gilsons.org>
2017-07-10 12:22:31 -0400

    purple again, better tmux colors

--- root/bin/st-0.7/config.h
+++ root/bin/st-0.7/config.h
@@ -82,7 +82,7 @@ static char termname[] = "st-256color";
  */
 static unsigned int tabspaces = 8;
 
-#include "/home/jonathan/.colors/solarized_dark.h"
+#include "/home/jonathan/.colors/paraiso_dark.h"
 
 /*
  * Default shape of cursor
