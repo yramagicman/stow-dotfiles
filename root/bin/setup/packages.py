10a2f846a027f1bbba66101e694bad9bfa44c162
Jonathan <jonathandavis@gilsons.org>
2017-08-31 12:54:22 -0400

    make sure repos aren't already there

--- root/bin/setup/packages.py
+++ root/bin/setup/packages.py
@@ -1,10 +1,19 @@
 import subprocess
+ar = ['zypper', 'ar', '-f']
+lr = ['zypper', 'lr', '-u']
+zypper = ['zypper', 'in' ]
+
 default = open('./default_repos')
 default_list = default.readlines()
 default_list  = [p.strip() for p in default_list]
 default.close()
-zypper = ['zypper', 'in' ]
-ar = ['zypper', 'ar', '-f']
+
+repos = subprocess.check_output(lr).decode().split('|')
+repos = [r.strip() for r in repos if 'http' in r]
+repos = [r[:-2].strip() for r in repos if 'http' in r]
+packman = 'http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/'
+dvd = 'http://opensuse-guide.org/repo/openSUSE_Leap_42.3/'
+
 subprocess.call(zypper + default_list)
 
 packman = open('./packman')
@@ -12,18 +21,21 @@ packman_list = packman.readlines()
 packman_list = [p.strip() for p in packman_list]
 packman.close()
 
-packman = 'http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/'
 
-subprocess.call(ar + [packman, 'packman'] )
-subprocess.call(['zypper', 'refresh'])
+
+if packman in repos:
+    subprocess.call(ar + [packman, 'packman'] )
+    subprocess.call(['zypper', 'refresh'])
+
 subprocess.call(zypper + ['--from', 'packman'] + packman_list)
 
 dvd = open('./dvd')
 dvd_list = dvd.readlines()
 dvd_list  = [p.strip() for p in dvd_list]
 dvd.close()
-dvd = 'http://opensuse-guide.org/repo/openSUSE_Leap_42.3/'
 
-subprocess.call(ar + [dvd, 'dvd'] )
+if dvd in repos:
+    subprocess.call(ar + [dvd, 'dvd'] )
+    subprocess.call(['zypper', 'refresh'])
 
 subprocess.call(zypper + ['--from', 'dvd'] + dvd_list)
