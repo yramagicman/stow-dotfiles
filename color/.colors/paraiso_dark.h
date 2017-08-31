15af3f635b8794b83657c64f6c55261820e120f0
Jonathan <jonathandavis@gilsons.org>
2017-08-31 10:59:43 -0400

    use git version of st

--- color/.colors/paraiso_dark.h
+++ color/.colors/paraiso_dark.h
@@ -1,5 +1,5 @@
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
+const char *colorname[] = {
 
   /* 8 normal colors */
   [0] = "#2f1e2e", /* black   */
@@ -30,15 +30,15 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-static unsigned int defaultfg = 257;
-static unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+unsigned int defaultrcs = 256;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
  * will reverse too. Another logic would only make the simple feature too
  * complex.
  */
-static unsigned int defaultitalic = 7;
-static unsigned int defaultunderline = 7;
+unsigned int defaultitalic = 7;
+unsigned int defaultunderline = 7;
