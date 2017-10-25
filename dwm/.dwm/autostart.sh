e8be68eca5b714ae47a28dd5c2a57b5a13d6361d
Jonathan <jonathandavis@gilsons.org>
2017-10-25 13:39:17 -0400

    shellcheck stuff

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -2,7 +2,7 @@
 xbacklight -set 25
 check_process(){
 
-    if [[ -z "$(pgrep $1 )" ]];
+    if  ! pgrep "$1";
     then
         $1 &
     fi
@@ -19,7 +19,7 @@ xset -dpms; xset s off &
 (sleep 1s && check_process compton -c) &
 (sleep 1s && /usr/bin/xscreensaver -no-splash) &
 (sleep 10s && xfce4-power-manager) &
-(sleep 5s  && $HOME/bin/get_remote_ip) &
+(sleep 5s  && "$HOME/bin/get_remote_ip") &
 
 ## Set keyboard settings - 250 ms delay and 25 cps (characters per
 ## second) repeat rate.  Adjust the values according to your
@@ -37,33 +37,34 @@ rm ~/.config/weather
 (sleep 45s && check_process redshift) &
 
 #limit the size of dirs history
-d=$(sort $HOME/.cache/zsh/dirs | uniq )
+d=$(sort -u  "$HOME/.cache/zsh/dirs" )
 rm "$HOME/.cache/zsh/dirs"
-echo "$d" > $HOME/.cache/zsh/dirs
+echo "$d" > "$HOME/.cache/zsh/dirs"
 
 # make sure tmux digests file isn't overly large
-z=$(cat $HOME/.tmux.d/digests | tail -n $(ls -1 $HOME/.tmux.d/ | wc -l) )
+z=$(tail  "$HOME/.tmux.d/digests" -n "$(find  "$HOME/.tmux.d/" | wc -l)" )
 rm "$HOME/.tmux.d/digests"
-echo "$z" > $HOME/.tmux.d/digests
+echo "$z" > "$HOME/.tmux.d/digests"
 
-echo "" > $HOME/.xsession-errors
-if ! stat $HOME/.xsession-errors.old > /dev/null; then
-    rm $HOME/.xsession-errors.old
+echo "" > "$HOME/.xsession-errors"
+if ! stat "$HOME/.xsession-errors.old" > /dev/null; then
+    rm "$HOME/.xsession-errors.old"
 fi
-if ! stat $HOME/Gits/st/st > /dev/null; then
+if ! stat "$HOME/Gits/st/st" > /dev/null; then
     notify-send -u critical "st not intsalled in $HOME/Gits/st"
 fi
 
 if test "$( hostname )" = 'k-nine'; then
     if nmcli | grep 'hide_yo_kids'; then
-        $HOME/bin/get_remote_ip
+        "$HOME/bin/get_remote_ip"
     fi
 
-    if ! grep "$(cat $HOME/.config/rip)" /etc/sysconfig/proxy; then
+    if ! grep "$(cat "$HOME/.config/rip")" /etc/sysconfig/proxy; then
         notify-send -u critical "Remote IP and Proxy URL are different"
     fi
 fi
 fetchmail &
-echo '' > $HOME/.config/fetchmail.log
-echo '' > $HOME/.config/procmail.log
+echo '' > "$HOME/.config/fetchmail.log"
+echo '' > "$HOME/.config/procmail.log"
+echo '' > "$HOME/.config/msmtp.log"
 exit
