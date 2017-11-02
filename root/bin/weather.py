9599b999791472c9ead9088e51d7d1cdfb90df2a
Jonathan <jonathandavis@gilsons.org>
2017-11-01 22:00:05 -0400

    formatting

--- root/bin/weather.py
+++ root/bin/weather.py
@@ -6,5 +6,5 @@ import json
 j = json.JSONDecoder()
 r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=45449,us&units=imperial&appid=8c7a3eda698041f3d194181a7b1da8c5')
 
-weather =  r.json()
+weather = r.json()
 print(weather['main']['temp'])
