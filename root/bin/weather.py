668b71adf7cdc320dd09ff42b559e9b10670c76c
Jonathan <jonathandavis@gilsons.org>
2017-11-02 11:03:29 -0400

    make sure weather gets updated

--- root/bin/weather.py
+++ root/bin/weather.py
@@ -11,14 +11,19 @@ r = requests.get(
     'http://api.openweathermap.org/data/2.5/weather?zip=45449,us&units=imperial&appid=8c7a3eda698041f3d194181a7b1da8c5')
 
 weather = r.json()
-display = ''
-if hostname == 'geeko':
-    display = display + weather['weather'][0]['main']
-    if weather['weather'][0]['main'] != weather['weather'][1]['main']:
-        display = display + ' or '
-        display = display + weather['weather'][1]['main']
-    display = display + ' '
-
+try:
+    display = ''
+    if hostname == 'geeko':
+        display = display + weather['weather'][0]['main']
+        if weather['weather'][0]['main'] != weather['weather'][1]['main']:
+            display = display + ' or '
+            display = display + weather['weather'][1]['main']
+        display = display + ' '
+except:
+    display = ''
+    if hostname == 'geeko':
+        display = display + weather['weather'][0]['main']
+        display = display + ' '
 display = display + str(weather['main']['temp'])
 display = display + 'F'
 print(display)
