1bf70ed4d01af61c26d0398f25e79384f6c864ea
Jonathan <jonathandavis@gilsons.org>
2017-12-12 21:56:00 -0500

    kill base 16

--- emacs/.emacs.d/lisp/custom.el
+++ emacs/.emacs.d/lisp/custom.el
@@ -8,7 +8,6 @@
  '(ac-show-menu-immediately-on-auto-complete t)
  '(ac-trigger-key "TAB")
  '(column-number-mode t)
- '(custom-enabled-themes (quote (base16-ocean)))
  '(delete-selection-mode nil)
  '(display-time-mode t)
  '(electric-pair-mode t)
@@ -23,7 +22,7 @@
  '(mode-require-final-newline nil)
  '(package-selected-packages
    (quote
-    (helm-flycheck helm key-chord git-commit base16-theme helm-fuzzy-find helm-git helm-projectile evil-escape general ac-php-core php-mode epc auto-complete evil-numbers evil-nerd-commenter evil-visualstar evil-visual-star evil-surround web-mode scss-mode sass-mode relative-line-numbers racket-mode projectile php-eldoc muttrc-mode markdown-mode magit jedi flycheck evil-matchit evil clojure-mode ac-php)))
+    (helm-flycheck helm key-chord git-commit helm-fuzzy-find helm-git helm-projectile evil-escape general ac-php-core php-mode epc auto-complete evil-numbers evil-nerd-commenter evil-visualstar evil-visual-star evil-surround web-mode scss-mode sass-mode relative-line-numbers racket-mode projectile php-eldoc muttrc-mode markdown-mode magit jedi flycheck evil-matchit evil clojure-mode ac-php)))
  '(require-final-newline nil)
  '(save-place t)
  '(scroll-bar-mode nil)
