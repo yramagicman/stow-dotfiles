926445d2365475260943cd1f134086fe8bb08d72
Jonathan <jonathandavis@gilsons.org>
2017-12-18 00:23:42 -0500

    date and such

--- zsh/.zsh/parallel.zsh
+++ zsh/.zsh/parallel.zsh
@@ -2,9 +2,10 @@
 
 for p in $PACKAGES
 do
-    if [[ ! -a $p ]];
+    if [[ ! -a $p && ! -d "$MODULES_DIR/$p" ]];
     then
         mkdir -p "$MODULES_DIR/$p"
-        git clone --depth 3 "https://github.com/$p"  "$MODULES_DIR/$p" &
+        git clone --quiet --depth 3 "git@github.com:$p"  "$MODULES_DIR/$p" &
     fi
 done
+date +'%s' > $MODULES_DIR/.updatetime
