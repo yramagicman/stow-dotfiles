1a042f4a8998e5c20a02c885c2817ab19048ec1e
Jonathan <jonathandavis@gilsons.org>
2017-12-18 00:12:54 -0500

    now it clones stuff right

--- zsh/.zsh/parallel.zsh
+++ zsh/.zsh/parallel.zsh
@@ -4,8 +4,7 @@ for p in $PACKAGES
 do
     if [[ ! -a $p ]];
     then
-        mkdir -p "$HOME/ptest/$p"
-        git clone --depth 3 "https://github.com/$p" "$HOME/ptest/$p" &
+        mkdir -p "$MODULES_DIR/$p"
+        git clone --depth 3 "https://github.com/$p"  "$MODULES_DIR/$p" &
     fi
 done
-tree ptest
